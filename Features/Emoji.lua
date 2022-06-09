74cb62000434e130038a7df8469d7d649b2a70eb
Scrvpter <93370230+Scrvpter@users.noreply.github.com>
2022-06-09 20:58:52 +0800

    Update Emoji.lua

--- Features/Emoji.lua
+++ Features/Emoji.lua
@@ -1,27 +1,35 @@
-local StarId = {
-    [2470610118] = true,
-}
+local StarId = {}
 
 local CrownId = {
-    [244946092] = true,
+	[244946092] = true,
 }
 
-function xdlovepussy()
-    for _,v in pairs(game:GetService('Players'):GetChildren()) do
-        if StarId[v.UserId] then
-            if v.Character then
-                if v.Character.Parent.Name == 'Players' then
-                    v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
-                end
-            end
-        elseif CrownId[v.UserId] then
-            if v.Character then
-                if v.Character.Parent.Name == 'Players' then
-                    v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]'..v.DisplayName)
-                end
-            end
-        end
-    end
+local DiamondId = {
+	[2470610118] = true,
+}
+
+function premium()
+	for _,v in pairs(game:GetService('Players'):GetChildren()) do
+		if StarId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
+				end
+			end
+		elseif CrownId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[Owner] '..v.DisplayName)
+				end
+			end
+		elseif DiamondId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🥵]'..v.DisplayName)
+				end
+			end
+		end
+	end
 end
-local success,err = pcall(xdlovepussy)
+local success,err = pcall(premium)
 return StarId
